c507c8b8e63411bbeab6600b675143874c84dc7f
Kenny Nguyen <kennamngu@gmail.com>
2024-05-29 11:16:50 +0200

    Added landing page

---
+++ src/links.ts
@@ -0,0 +1,22 @@
+export const links = [
+  {
+    id: 1,
+    name: "#Sanity Create",
+    url: "https://www.sanity.io/app/create?ref=lp",
+  },
+  {
+    id: 2,
+    name: "#Sanity Learn",
+    url: "https://www.sanity.io/learn",
+  },
+  {
+    id: 3,
+    name: "#High Frequency CDN",
+    url: "https://www.sanity.io/blog/recap-sanity-connect-and-latest-releases#Introducing%20the%20High-frequency%20CDN",
+  },
+  {
+    id: 4,
+    name: "#Typegen",
+    url: "/",
+  },
+];
